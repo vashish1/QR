aabab805f21abda740b9633292ad9e89a200f726
vashish1 <vashishtiv@gmail.com>
2020-07-29 00:29:03 +0530

    adding files

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"image/png"
+	"net/http"
+	"strings"
+	"text/template"
+
+	"github.com/boombuler/barcode"
+	"github.com/boombuler/barcode/qr"
+	"github.com/gorilla/mux"
+)
+
+
+func main() {
+	r:=mux.NewRouter()
+
+	r.HandleFunc("/QR", homeHandler).Methods("GET")
+	r.HandleFunc("/QR/Generated", viewCodeHandler).Methods("POST","GET")
+	http.Handle("/",r)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+
+	t, _ := template.ParseFiles("./index.html")
+	t.Execute(w, "")
+}
+
+func viewCodeHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	text := r.FormValue("test")
+	link := r.FormValue("link")
+	if strings.ToLower(link)=="no"{
+		link=""
+	}
+	Data:= text+ " \n "+ link +"\n \n ~ Made by " +name + " :-)"
+	qrCode, _ := qr.Encode(Data, qr.L, qr.Auto)
+	qrCode, _ = barcode.Scale(qrCode, 128, 128)
+
+	png.Encode(w, qrCode)
+}
